6b58cdec71904524ccbe34ad5da502df26f8a7c1
jisukim97 <54925185+jisukim97@users.noreply.github.com>
2021-07-04 18:16:55 +0900

    new start

---
+++ 1.1부터 N까지 M의배수 합.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+
+    int n, m, sum=0;
+    cin >> n >> m;
+
+    for (int i = m; i <= n; i+=m) 
+            sum += i;
+    
+    cout << sum;
+
+    return 0;
+}
---
+++ 2.자연수의 합.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    int a, b, i, sum=0;
+    cin >> a >> b;
+
+    for (i = a; i < b; i++) {
+        cout << i << " +";
+        sum += i;
+    }
+    sum += i;
+    cout << i << " = " << sum;
+      
+
+    return 0;
+}
